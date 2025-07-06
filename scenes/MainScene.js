99dabc2414c8c52d53802eebde562472879034b6
Rusty Bentley <mycoding.alert@gmail.com>
2025-07-06 13:24:29 -0700

    adds sprites and direction flip

--- scenes/MainScene.js
+++ scenes/MainScene.js
@@ -4,7 +4,8 @@ export default class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    // this.load.image('player', 'assets/player.png');
+    this.load.image('chase', 'assets/chase.png');
+    this.load.image('garner', 'assets/garner.png');
   }
 
   create() {
@@ -12,14 +13,17 @@ export default class MainScene extends Phaser.Scene {
     this.physics.add.existing(this.ground, true);
 
     // TODO: replace rectangular players with images
-    this.player1 = this.add.rectangle(200, 400, 40, 60, 0x00ff00);
-    this.player2 = this.add.rectangle(600, 400, 40, 60, 0xff0000);
-    this.physics.add.existing(this.player1);
-    this.physics.add.existing(this.player2);
+    // this.player1 = this.add.rectangle(200, 400, 40, 60, 0x00ff00);
+    // this.player2 = this.add.rectangle(600, 400, 40, 60, 0xff0000);
+    // this.physics.add.existing(this.player1);
+    // this.physics.add.existing(this.player2);
+
+    this.player1 = this.physics.add.sprite(200, 400, 'chase');
+    this.player2 = this.physics.add.sprite(600, 400, 'garner');
+    this.player1.setCollideWorldBounds(true);
+    this.player2.setCollideWorldBounds(true);
     this.physics.add.collider(this.player1, this.ground);
     this.physics.add.collider(this.player2, this.ground);
-    this.player1.body.setCollideWorldBounds(true);
-    this.player2.body.setCollideWorldBounds(true);
 
     // note that the player has to be added to the physics engine in order to access setBounce 
     this.player1.body.setBounce(0.2);
@@ -66,5 +70,8 @@ export default class MainScene extends Phaser.Scene {
     if (this.p2Controls.up.isDown && p2.blocked.down) {
       p2.setVelocityY(-400);
     }
+
+    this.player1.setFlipX(this.player1.body.velocity.x < 0);
+    this.player2.setFlipX(this.player2.body.velocity.x < 0);
   }
 }
